f6b07c8ba7353dede7adf5eb2f431e47008e06f5
Wyatt James <wyattsamueljames@gmail.com>
2024-10-31 08:29:15 -0600

    feat(pokemon) add init for Card

--- src/pokemon/card.py
+++ src/pokemon/card.py
@@ -9,3 +9,9 @@ class Card:
     set_code: str
     card_set_number: int
     rarity: Rarity
+
+    def __init__(self, name: str, set_code: str, card_set_number: int, rarity: str) -> None:
+        self.name = name
+        self.set_code = set_code
+        self.card_set_number = card_set_number
+        self.rarity =
