061f474f009d824a322e861b4ebd46228f298b1e
Wyatt James <wyattsamueljames@gmail.com>
2024-11-01 12:42:39 -0600

    feat(pokemon): use parse_rarity to handle card class member in init

--- src/pokemon/card.py
+++ src/pokemon/card.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from src.pokemon.rarity import Rarity
+from src.pokemon.rarity import Rarity, parse_rarity
 
 
 @dataclass
@@ -10,8 +10,10 @@ class Card:
     card_set_number: int
     rarity: Rarity
 
-    def __init__(self, name: str, set_code: str, card_set_number: int, rarity: str) -> None:
+    def __init__(
+        self, name: str, set_code: str, card_set_number: int, rarity: str
+    ) -> None:
         self.name = name
         self.set_code = set_code
         self.card_set_number = card_set_number
-        self.rarity =
+        self.rarity = parse_rarity(rarity)
