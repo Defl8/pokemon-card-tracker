de100c476ec5ab00366cb67a62be70e8d89c81ed
Wyatt James <wyattsamueljames@gmail.com>
2024-11-06 17:34:16 -0700

    feat(pokemon): add init for Energy class

--- src/pokemon/energy.py
+++ src/pokemon/energy.py
@@ -5,3 +5,16 @@ from src.pokemon.card import Card
 
 @dataclass
 class Energy(Card):
+
+    energy_type: str
+
+    def __init__(
+        self,
+        name: str,
+        set_code: str,
+        card_set_number: int,
+        rarity: str,
+        energy_type: str,
+    ) -> None:
+        super().__init__(name, set_code, card_set_number, rarity)
+        self.energy_type = energy_type
