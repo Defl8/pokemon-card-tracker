12dda6b18bedaf84290f9d361f9a2df3d8a2ca52
Wyatt James <wyattsamueljames@gmail.com>
2024-11-06 17:38:50 -0700

    chore(pokemon): initial commit

---
+++ src/pokemon/energy_type.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class EnergyType(Enum):
+    NA = -1
+    COMMON = 0
+    UNCOMMON = 1
+    RARE = 2
+    REVERSE_HOLOFOIL = 3
+    HOLOFOIL_RARE = 4
+    DOUBLE_RARE = 5
+    ULTRA_RARE = 6
+    SECRET_RARE = 7
