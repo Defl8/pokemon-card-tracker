f3d60662c0686d6a317f9723849fb97c3feda8a7
Wyatt James <wyattsamueljames@gmail.com>
2024-11-08 10:01:14 -0700

    feat(pokemon): add type attribute to Card

--- src/pokemon/card.py
+++ src/pokemon/card.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from src.pokemon.enums import Rarity, string_to_enum_member
+from src.pokemon.enums import Rarity, Type, string_to_enum_member
 
 
 @dataclass
@@ -10,12 +10,14 @@ class Card:
     name: str
     set_code: str
     card_set_number: int
+    type: Type
     rarity: Rarity
 
     def __init__(
-        self, name: str, set_code: str, card_set_number: int, rarity: str
+        self, name: str, set_code: str, card_set_number: int, type: str, rarity: str
     ) -> None:
         self.name = name
         self.set_code = set_code
         self.card_set_number = card_set_number
         self.rarity = string_to_enum_member(rarity, Rarity)
+        self.type = string_to_enum_member(type, Type)
