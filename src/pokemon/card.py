975220611275c13f54afb5c67b43619eddf1ebbf
Wyatt James <wyattsamueljames@gmail.com>
2024-11-08 11:30:55 -0700

    feat(pokemon): add amount to Card attributes

--- src/pokemon/card.py
+++ src/pokemon/card.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from src.pokemon.enums import Rarity, Type, string_to_enum_member
+from src.db.sql import insert_into_table
 
 
 @dataclass
@@ -12,12 +13,23 @@ class Card:
     card_set_number: int
     type: Type
     rarity: Rarity
+    amount: int
 
     def __init__(
-        self, name: str, set_code: str, card_set_number: int, type: str, rarity: str
+        self,
+        name: str,
+        set_code: str,
+        card_set_number: int,
+        type: str,
+        rarity: str,
+        amount: int,
     ) -> None:
         self.name = name
         self.set_code = set_code
         self.card_set_number = card_set_number
         self.rarity = string_to_enum_member(rarity, Rarity)
         self.type = string_to_enum_member(type, Type)
+        self.amount = amount
+
+    def write_to_db(self, db_name: str):
+        insert_statement: str = "insert into cards(name)"
