27ad18a4ca9a61d6cf27ad999a7b533270ab2e4e
Wyatt James <wyattsamueljames@gmail.com>
2024-12-11 12:08:01 -0700

    refactor(move) update Attack attrs

--- src/cards/move.py
+++ src/cards/move.py
@@ -12,9 +12,14 @@ class Move:
 # TODO: make cost object
 @dataclass
 class Attack(Move):
+    _cost: str  # TODO: change to obj
+    _effect: str | None
+    _damage: int | None
+
     def __init__(
-        self,
-        name: str,
-        cost: str,
+        self, name: str, cost: str, effect: str | None, damage: int | None
     ) -> None:
         super().__init__(name)
+        self._cost = cost  # TODO: Should be Obj in the future
+        self._effect = effect  # Not all moves have an effect
+        self._damage = damage  # Not all moves do damage
