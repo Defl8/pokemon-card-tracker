bda7c1acbe1b15ab052a27c9fcb3d7f01b3cbf65
Wyatt James <wyattsamueljames@gmail.com>
2024-12-11 11:00:29 -0700

    feat(move): add Attack class

--- src/cards/move.py
+++ src/cards/move.py
@@ -3,5 +3,18 @@ from dataclasses import dataclass
 
 @dataclass
 class Move:
-    def __init__(self) -> None:
-        pass
+    _name: str
+
+    def __init__(self, name: str) -> None:
+        self._name = name
+
+
+# TODO: make cost object
+@dataclass
+class Attack(Move):
+    def __init__(
+        self,
+        name: str,
+        cost: str,
+    ) -> None:
+        super().__init__(name)
