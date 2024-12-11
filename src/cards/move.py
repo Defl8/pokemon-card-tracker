cf7a4b403a9ddbc2cff384f31cc1836121f47a4c
Wyatt James <wyattsamueljames@gmail.com>
2024-12-11 12:11:27 -0700

    feat(move): add Ability class

--- src/cards/move.py
+++ src/cards/move.py
@@ -12,14 +12,23 @@ class Move:
 # TODO: make cost object
 @dataclass
 class Attack(Move):
-    _cost: str  # TODO: change to obj
+    _cost: str | None  # TODO: change to obj, some move don't have cost
     _effect: str | None
     _damage: int | None
 
     def __init__(
-        self, name: str, cost: str, effect: str | None, damage: int | None
+        self, name: str, cost: str | None, effect: str | None, damage: int | None
     ) -> None:
         super().__init__(name)
         self._cost = cost  # TODO: Should be Obj in the future
         self._effect = effect  # Not all moves have an effect
         self._damage = damage  # Not all moves do damage
+
+
+@dataclass
+class Ability(Move):
+    _effect: str | None
+
+    def __init__(self, name: str, effect: str) -> None:
+        super().__init__(name)
+        self._effect = effect
