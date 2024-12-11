f00594d9e040388c2b743943cd21cbd15fdfc53c
Wyatt James <wyattsamueljames@gmail.com>
2024-12-11 12:49:34 -0700

    feat(card): add set attr

--- src/cards/card.py
+++ src/cards/card.py
@@ -1,8 +1,13 @@
 class Card:
+    _type: str = ""
+    _set: str = ""
+    _allowed_card_types: tuple[str, ...] = ("pokemon", "trainer", "energy")
+    _max_set_len: int = 3
+
     def __init__(self, card_type: str, name: str, set: str) -> None:
-        self._type: str = ""
+        self.type = card_type
         self.name: str = name
-        self._set: str = set
+        self.set = set
 
     @property
     def type(self) -> str:
@@ -10,7 +15,16 @@ class Card:
 
     @type.setter
     def type(self, card_type: str) -> None:
-        allowed_card_types: tuple[str, ...] = ("pokemon", "trainer", "energy")
-        if card_type.strip() not in allowed_card_types:
+        if card_type.strip() not in Card._allowed_card_types:
             raise ValueError(f"Value {card_type} is not a valid card type.")
         self._type = card_type
+
+    @property
+    def set(self) -> str:
+        return self._set
+
+    @set.setter
+    def set(self, set: str) -> None:
+        if len(set) > Card._max_set_len:
+            raise ValueError(f"Input set name {set} is too long.")
+        self._set = set
--- src/cards/pokemon.py
+++ src/cards/pokemon.py
@@ -7,6 +7,7 @@ class Pokemon(Card):
         self,
         card_type: str,  # inherited
         name: str,  # inherited
+        set: str,  # inherited
         pkmn_type: str,
         stage: str,
         hit_points: int,
@@ -14,7 +15,7 @@ class Pokemon(Card):
         weakness: str,  # TODO: make energy object
         *moves: Attack | Ability,
     ) -> None:
-        super().__init__(card_type, name)
+        super().__init__(card_type, name, set)
         self.pkmn_type: str = pkmn_type
         self.stage: str = stage
         self.hit_points: int = hit_points
