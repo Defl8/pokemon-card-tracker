14b7015ad78744afe3bf8596c85af4efee7f1999
Wyatt James <wyattsamueljames@gmail.com>
2024-12-11 09:45:00 -0700

    chore: initial commit, blank project

--- src/error/exceptions.py
+++
@@ -1,8 +0,0 @@
-class InvalidCardType(Exception):
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
-
-
-class InvalidCardRarity(Exception):
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
