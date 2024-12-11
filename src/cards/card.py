5214f205f6f7d96c6df7352cc280e70fed456f36
Wyatt James <wyattsamueljames@gmail.com>
2024-12-11 12:38:58 -0700

    refactor(card): update attrs

--- src/cards/card.py
+++ src/cards/card.py
@@ -1,15 +1,16 @@
 class Card:
-    def __init__(self, card_type: str, name: str) -> None:
-        self._card_type: str = card_type
+    def __init__(self, card_type: str, name: str, set: str) -> None:
+        self._type: str = ""
         self.name: str = name
+        self._set: str = set
 
     @property
     def type(self) -> str:
-        return self._card_type
+        return self._type
 
     @type.setter
     def type(self, card_type: str) -> None:
         allowed_card_types: tuple[str, ...] = ("pokemon", "trainer", "energy")
         if card_type.strip() not in allowed_card_types:
             raise ValueError(f"Value {card_type} is not a valid card type.")
-        self._card_type = card_type
+        self._type = card_type
