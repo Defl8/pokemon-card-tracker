ca058d97bc67023bc0bde82759274db9793e13aa
Wyatt James <wyattsamueljames@gmail.com>
2024-12-11 10:51:36 -0700

    docs(pokemon): update comments

--- src/cards/pokemon.py
+++ src/cards/pokemon.py
@@ -4,15 +4,13 @@ from src.cards.card import Card
 class Pokemon(Card):
     def __init__(
         self,
-        card_type: str,
-        name: str,
+        card_type: str,  # inherited
+        name: str,  # inherited
         pkmn_type: str,
         stage: str,
         hit_points: int,
     ) -> None:
         super().__init__(card_type, name)
-        self._card_type: str = card_type
-        self.name: str = name
         self.pkmn_type: str = pkmn_type
         self.stage: str = stage
         self.hit_points: int = hit_points
