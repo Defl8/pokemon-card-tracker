86d1a9af68fb22bdb70a590c877e3e6be0f01ae6
Wyatt James <wyattsamueljames@gmail.com>
2024-12-12 07:57:03 -0700

    feat(card): add reg_mark attr

--- src/cards/card.py
+++ src/cards/card.py
@@ -1,30 +1,7 @@
 class Card:
-    _allowed_card_types: tuple[str, ...] = ("pokemon", "trainer", "energy")
-    _max_set_len: int = 3
-
-    def __init__(self, card_type: str, name: str, set: str) -> None:
+    def __init__(self, card_type: str, name: str, set: str, reg_mark: str) -> None:
         self._type: str = ""
-        self.type = card_type
+        self.type: str = card_type
         self.name: str = name
-        self._set: str = ""
-        self.set = set
-
-    @property
-    def type(self) -> str:
-        return self._type
-
-    @type.setter
-    def type(self, card_type: str) -> None:
-        if card_type.strip() not in Card._allowed_card_types:
-            raise ValueError(f"Value {card_type} is not a valid card type.")
-        self._type = card_type
-
-    @property
-    def set(self) -> str:
-        return self._set
-
-    @set.setter
-    def set(self, set: str) -> None:
-        if len(set) > Card._max_set_len:
-            raise ValueError(f"Input set name {set} is too long.")
-        self._set = set
+        self.set: str = set
+        self.reg_mark: str = reg_mark
---
+++ src/cards/legality.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Legality:
+    standard: bool
+    expanded: bool
