fe746fcc4749ec2609f969922e7b89db9625ecb5
Wyatt James <wyattsamueljames@gmail.com>
2024-12-12 08:19:58 -0700

    feat(card): add legality attr

--- src/cards/card.py
+++ src/cards/card.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from src.cards.legality import Legality
+
 
 @dataclass
 class Card:
@@ -8,3 +10,5 @@ class Card:
     set: str
     reg_mark: str
     rarity: str
+    is_promo: bool
+    legality: Legality | None
--- src/cards/energy.py
+++ src/cards/energy.py
@@ -2,19 +2,6 @@ from src.cards.card import Card
 
 
 class Energy(Card):
-    _energy_types: tuple[str, ...] = (
-        "Fire",
-        "Water",
-        "Grass",
-        "Electric",
-        "Fighting",
-        "Darkness",
-        "Psychic",
-        "Metal",
-        "Fairy",
-        "Special",
-    )
-
     def __init__(
         self,
         card_type: str,
@@ -23,19 +10,11 @@ class Energy(Card):
         reg_mark: str,
         rarity: str,
         eng_type: str,
+        is_promo: bool,
         is_acespec: bool = False,
     ) -> None:
-        super().__init__(card_type, name, set, reg_mark, rarity)
-        self._eng_type: str = ""
-        self.eng_type = eng_type
+        super().__init__(
+            card_type, name, set, reg_mark, rarity, is_promo, legality=None
+        )
+        self.eng_type: str = eng_type
         self.is_acespec: bool = is_acespec
-
-    @property
-    def eng_type(self) -> str:
-        return self._eng_type
-
-    @eng_type.setter
-    def eng_type(self, eng_type: str) -> None:
-        if eng_type not in Energy._energy_types:
-            raise ValueError(f"Energy type {eng_type} is not valid.")
-        self._eng_type = "Special" if self.is_acespec else eng_type
