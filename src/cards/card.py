a87b1dabe43431cbc097d8fc1b624a2b6e9c8fe2
Wyatt James <wyattsamueljames@gmail.com>
2024-12-12 08:10:02 -0700

    feat(card): convert to dataclass

--- src/cards/card.py
+++ src/cards/card.py
@@ -1,7 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Card:
-    def __init__(self, card_type: str, name: str, set: str, reg_mark: str) -> None:
-        self._type: str = ""
-        self.type: str = card_type
-        self.name: str = name
-        self.set: str = set
-        self.reg_mark: str = reg_mark
+    name: str
+    type: str
+    set: str
+    reg_mark: str
